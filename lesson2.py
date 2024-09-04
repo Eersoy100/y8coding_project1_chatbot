86c999e56fc530875272e666612fedc1caab996c
Eersoy100 <eersoy@kellettschool.com>
2024-09-04 04:34:57 +0000

    lesson 2

---
+++ lesson2.py
@@ -0,0 +1,22 @@
+print("""1. TYPHOON
+      2. SUNNY
+      3. SNOWING""")
+weather = int(input("what is the weather like today?"))
+
+if weather == "1":
+    location = "inside"
+elif weather == "2":
+    location = "at the beach"
+elif weather == "3":
+    location = "skiing"
+
+    food = input("what are you eating?")
+
+    time = input("what is the time?")
+    
+    enjoyment = input("do you or do you not like the weather?")
+
+    enjoyment2 = input(f"why {enjoyment} you like the weather?")
+    
+    print(f"today you are {location} eating {food} and it is {time}. You {enjoyment} like the weather beacause {enjoyment2} ")
+
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# your chatbot code here!
+name = imput("enter your name"), 
+print(name "Ive been waiting all my life to meet you!")
+if name == "evren":
+    print("EVRENNNNNN!!!!!!!!!!!!!!!!")
+else:
+    print ("bye.")
+
+    siblings = int(imput("how many brothers and sisters do you have?"))
+
+    if siblings == 0:
+        print( "you are an only child.")
+    elif siblings >5:
+        print("well thats a big family")
